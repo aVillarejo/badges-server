014dffd37303b5684fa8badfa98b3de0cb7231c2
“a_villarejo19” <a.villarejo19@gmail.com>
2019-08-04 18:46:48 -0500

    Crud Operations for badges route

--- routes/badge.routes.js
+++ routes/badge.routes.js
@@ -1,22 +1,106 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const Badge = require('../models/badge');
 
-const Badge = require("../models/badge");
-
-router.post("/", async (req, res) => {
-  // const { fistName, lastName, email, jobTitle, twitter } = req.body;
+/**
+ * @route api/badges/
+ * @desc  Create a new badge
+ * @param  body fistName, lastName, email, jobTitle, twitter
+ */
+router.post('/', async (req, res) => {
   const { body } = req;
-
   await Badge.create(body, (err, doc) => {
     if (err) {
       return res.send({
         success: false,
-        message: "Error: Server error"
+        message: 'Error: Server error'
       });
     }
     return res.send({
       success: true,
-      message: "Success"
+      message: 'Success'
+    });
+  });
+});
+
+/**
+ * @route api/badges/
+ * @desc  Read all Badges
+ */
+router.get('/', async (req, res) => {
+  await Badge.find({}, (err, docs) => {
+    if (err) {
+      return res.send({
+        success: false,
+        message: 'Error: Server error'
+      });
+    }
+    return res.send({
+      data: docs,
+      success: true
+    });
+  });
+});
+
+/**
+ * @route api/badges/:id
+ * @desc  Read a badge
+ * @param  id badge id
+ */
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  await Badge.findById(id, (err, doc) => {
+    if (err) {
+      return res.send({
+        success: false,
+        message: 'Error: Server error'
+      });
+    }
+    return res.send(doc);
+  });
+});
+
+
+/**
+ * @route api/badges/:id
+ * @desc  Create a new badge
+ * @param  id Badge id to update
+ * @body badge data: fistName, lastName, email, jobTitle, twitter
+ */
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const {body} = req;
+
+  await Badge.findByIdAndUpdate(id,body,(err, doc) => {
+    if (err) {
+      return res.send({
+        success: false,
+        message: 'Error: Server error'
+      });
+    }
+    return res.send({
+      success: true,
+      message: 'Success'
+    });
+  });
+});
+
+/**
+ * @route api/badges/:id
+ * @desc  Delete a badge
+ * @param  id badge id
+ */
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  await Badge.findByIdAndDelete(id, (err, doc) => {
+    if (err) {
+      return res.send({
+        success: false,
+        message: 'Error: Server error'
+      });
+    }
+    return res.send({
+      success: true
     });
   });
 });
--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-import express from "express";
-import morgan from "morgan";
-import db from "./database";
+import express from 'express';
+import morgan from 'morgan';
+import './database';
 
 //* App
 const app = express();
 
 //* App Settings
-app.set("PORT", process.env.PORT || 5000);
+app.set('PORT', process.env.PORT || 5000);
 const env = process.env.NODE_ENV;
 
 //* App Middlewares
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(express.json());
 
 //* Routes
-app.get("/api", (req, res) => {
-  res.send("Hello World!!");
+app.get('/api', (req, res) => {
+  res.send('Hello World!!');
 });
-app.use("/api/badges", require("./routes/badge.routes"));
+app.use('/api/badges', require('./routes/badge.routes'));
 
 //* Starting de Server
-app.listen(app.get("PORT"), () => {
-  console.log(`Server running on port ${app.get("PORT")} 🔥`);
+app.listen(app.get('PORT'), () => {
+  console.log(`Server running on port ${app.get('PORT')} 🔥`);
   console.log(`NODE_ENV: ${env}`);
 });
